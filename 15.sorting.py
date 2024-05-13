25324bba5e0dbbcf548c13a97ad79b01306c53ee
Satyam Jha <154743842+jha06sm@users.noreply.github.com>
2024-05-13 13:38:35 +0530

    Create 15.sorting.py

---
+++ 15.sorting.py
@@ -0,0 +1,9 @@
+s1=int(input("enter marks of student 1:"))
+s2=int(input("enter marks of student 2:"))
+s3=int(input("enter marks of student 3:"))
+s4=int(input("enter marks of student 4:"))
+s5=int(input("enter marks of student 5:"))
+s6=int(input("enter marks of student 6:"))
+markslist=[s1,s2,s3,s4,s5,s6]
+markslist.sort()
+print(markslist)
